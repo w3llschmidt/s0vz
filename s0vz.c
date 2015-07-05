eadbd4198a1583954c257dfeedfeec9734558bdb
Bernd Gewehr <bgewehr@users.noreply.github.com>
2015-07-05 08:27:42 +0200

    Update s0vz.c used double data type for aggmin

--- s0vz.c
+++ s0vz.c
@@ -53,7 +53,7 @@ void signal_handler(int sig);
 void daemonize(char *rundir, char *pidfile);
 
 int pidFilehandle, vzport, len, running_handles, rc;
-float aggmin;
+double aggmin;
 
 const char *vzserver, *vzpath, *vzuuid[64];
 
